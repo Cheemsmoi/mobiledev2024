178939c04131f4b6b0fc24324585f12dd2081553
Cheemsmoi <buivietan2774@gmail.com>
2024-09-17 07:58:31 +0700

    lab10
    still yet working

--- USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherActivity.java
+++ USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherActivity.java
@@ -4,8 +4,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class WeatherActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     public WeatherActivity() {
         super();
     }
@@ -19,7 +28,21 @@ public class WeatherActivity extends AppCompatActivity {
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         Log.i("onCreate", "onCreate State");
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
         setContentView(R.layout.activity_main);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        WeatherAdapter adapter = new WeatherAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new WeatherAndForecastFragment(), "Ha Noi");
+        adapter.addFragment(new WeatherAndForecastFragment(), "Paris");
+        adapter.addFragment(new WeatherAndForecastFragment(), "Moscow");
+        viewPager.setAdapter(adapter);
+
         // Create a new Fragment to be placed in the activity
         ForecastFragment firstFragment = new ForecastFragment();
         // Add the fragment to the 'container' FrameLayout
--- USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherAndForecastFragment.java
+++ USTH_weather/app/src/main/java/vn/usth/edu/weather/WeatherAndForecastFragment.java
@@ -1,14 +1,9 @@
-package vn.usth.edu.weather;
+package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import android.graphics.Color;
 
-import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,10 +11,11 @@ import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ForecastFragment#newInstance} factory method to
+ * Use the {@link WeatherAndForecastFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ForecastFragment extends Fragment {
+public class WeatherAndForecastFragment extends Fragment {
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +25,7 @@ public class ForecastFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ForecastFragment() {
+    public WeatherAndForecastFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +35,11 @@ public class ForecastFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment WeatherAndForecastFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ForecastFragment newInstance(String param1, String param2) {
-        ForecastFragment fragment = new ForecastFragment();
+    public static WeatherAndForecastFragment newInstance(String param1, String param2) {
+        WeatherAndForecastFragment fragment = new WeatherAndForecastFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,29 +59,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        LinearLayout linearLayout = new LinearLayout(getActivity());
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT
-        ));
-
-        TextView textView = new TextView(getActivity());
-        textView.setText("Thursday");
-        textView.setTextSize(18);
-        textView.setTextColor(Color.BLACK);
-        linearLayout.addView(textView);
-
-        ImageView imageView = new ImageView(getActivity());
-        imageView.setImageResource(R.drawable.weather_icon_1);
-        imageView.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-        ));
-        linearLayout.addView(imageView);
-
-        linearLayout.setBackgroundColor(0xCC7DDEEA);
-
-        return linearLayout;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_and_forecast, container, false);
     }
 }
